433fbbba45ef46cd5aa5f95b0b1581580baed8c3
Andrew Giang <giang184@gmail.com>
2021-06-07 16:17:31 -0700

    add codes to list the words in descending order

--- js/scripts.js
+++ js/scripts.js
@@ -19,18 +19,20 @@ $(document).ready(function() {
         count ++;
       }
       else{
-        $("ul").prepend("<li>" + count + ", " + temp + "</li>");
+        //$("ul").prepend("<li>" + count + ", " + temp + "</li>");
         newArray.push(count + ", " + temp);
         count = 1;
         temp = word;
       }
     });
-    $("ul").prepend("<li>" + count + ", " + temp + "</li>");
+    //$("ul").prepend("<li>" + count + ", " + temp + "</li>");
     newArray.push(count + ", " + temp);
-    console.log(newArray);
     newArray.sort();
-    newArray.reverse();
-    console.log(newArray);
+
+
+    newArray.forEach(function(word){
+      $("ul").prepend("<li>" + word.substring(3) + ", " + word.substring(0,1)+"</li>");
+    })
 
     $("#hidden").show();
     $("#click").hide();
